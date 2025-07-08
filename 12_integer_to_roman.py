7fa98cfa8b5890caffe30aeb8a29435b4207964f
Michael Vasiljev <gnu.xinm@gmail.com>
2025-07-08 09:37:17 +0300

    Add 12_integer_to_roman.py

--- 12_integer_to_roman.py
+++ 12_integer_to_roman.py
@@ -1,8 +1,5 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        def _convert_num(num: int) -> str:
-            return ''
-
         value_symbols = [
             (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
             (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'),
