9303152d73d1b5dd965efaa21bcadd11adb4d945
Michael Vasiljev <gnu.xinm@gmail.com>
2025-07-14 13:59:38 +0300

    Add 6_zigzag_conversion.py

---
+++ 6_zigzag_conversion.py
@@ -0,0 +1,24 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        total_jump = 2 * (numRows - 1)
+        result = []
+        for row in range(numRows):
+            if row in (0, numRows - 1):
+                first_jump, second_jump = total_jump, total_jump
+            else:
+                first_jump = total_jump - 2 * row
+                second_jump = total_jump - first_jump
+            index = row
+            jumps = 0
+            while index <= len(s) - 1:
+                result.append(s[index])
+                index += (not jumps % 2) * first_jump + (jumps % 2) * second_jump
+                jumps += 1
+        return ''.join(result)
+
+
+assert Solution().convert(s="PAYPALISHIRING", numRows=3) == "PAHNAPLSIIGYIR"
+assert Solution().convert(s="PAYPALISHIRING", numRows=4) == "PINALSIGYAHRPI"
+assert Solution().convert(s="A", numRows=1) == "A"
